4e1e24b20e6c8da722a72b4f6a6ad32f24102776
RamsesDuque <ramsesduque2001@gmail.com>
2023-11-28 00:11:55 -0400

    front changes

--- apps/authentication/forms.py
+++ apps/authentication/forms.py
@@ -12,14 +12,14 @@ class LoginForm(forms.Form):
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={
-                "placeholder": "Username",
+                "placeholder": "Usuario",
                 "class": "form-control"
             }
         ))
     password = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                "placeholder": "Password",
+                "placeholder": "Contraseña",
                 "class": "form-control"
             }
         ))
