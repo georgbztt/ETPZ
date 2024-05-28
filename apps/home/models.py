7aa457cf0d3a341dd3922df9eb13cafe2d72664e
douglasmontoya <douglasmontoyalbjf@gmail.com>
2024-05-27 22:26:19 -0400

    Agrega los modelos 'PeriodosAcademicos' y 'DatosPlantel'

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,109 +1,33 @@
-from email.policy import default
-from pyexpat import model
-from tokenize import blank_re
+
 from django.db import models
-from django.contrib.auth.models import User
-from django.core.validators import *
 
-class Estudiante(models.Model):
-    CI_OPT=(
-        ('V', 'V'),
-        ('E', 'E')
-    )
-
-    ci = models.CharField(max_length=10, validators=[MinLengthValidator(7), RegexValidator(r'^[0-9]{1,2}[.]?[0-9]{3}[.]?[0-9]{3}$')], unique=True, null=True)
-    ci_tipo = models.CharField(max_length=1, choices=CI_OPT, null=True)
-    nombre = models.CharField(max_length=30, null=True)
-    apellido = models.CharField(max_length=30, null=True)
-    lugar_de_nacimiento = models.CharField(max_length=30, null=True)
-    entidad_federal = models.CharField(max_length=30, null=True)
-    periodo = models.ForeignKey('home.Periodo', on_delete=models.SET_NULL, blank=True, null=True)#Relación a periodo para asignar carga y tomar en cuenta en carga de notas
-    periodo_completo = models.BooleanField(default=False)#Filtro para excluir de la carga de notas una vez fue cargada la nota
-    seccion = models.CharField(max_length=1, null=True)
-
-    class Meta:
-        verbose_name = ("Estudiante")
-        verbose_name_plural = ("Estudiantes")
+class PeriodosAcademicos(models.Model):
+    id = models.AutoField(primary_key=True)  # Llave primaria autoincremental
+    nombre = models.CharField(max_length=255, null=False)  # String de 255 de longitud
 
     def __str__(self):
-        return f'{self.nombre} {self.apellido}'
-
-    @property
-    def otros_periodos(self):
-        return Periodo.objects.filter(nota__estudiante=self).exclude(id=self.periodo_id).distinct()
-class Boleta(models.Model):
-
-    estudiante = models.ForeignKey('home.Estudiante', on_delete=models.SET_NULL, null=True)
-    fecha = models.DateField(null=True)
+        return self.nombre  # Representación en cadena del objeto
 
     class Meta:
-        verbose_name = ("Boleta")
-        verbose_name_plural = ("Boletas")
+        verbose_name_plural = "Periodos Académicos"  # Nombre en plural para el panel de administración
+
+
+class DatosPlantel(models.Model):
+    id = models.AutoField(primary_key=True)  # Llave primaria autoincremental
+    codigo = models.CharField(max_length=255)  # Cadena de texto no nula
+    nombre = models.CharField(max_length=255)  # Cadena de texto no nula
+    direccion = models.CharField(max_length=255)  # Cadena de texto no nula
+    telefono = models.PositiveIntegerField()  # Número no nulo
+    municipio = models.CharField(max_length=255)  # Cadena de texto no nula
+    entidad_federal = models.CharField(max_length=255)  # Cadena de texto no nula
+    zona_educativa = models.CharField(max_length=255)  # Cadena de texto no nula
+    distrito_escolar = models.CharField(max_length=255)  # Cadena de texto no nula
+    ci_tipo = models.CharField(max_length=1, null=False)  # String de un solo carácter no nulo
+    ci = models.PositiveIntegerField(null=False)  # Número no nulo
+    director = models.CharField(max_length=255)
 
     def __str__(self):
-        return f'{self.estudiante.nombre} {self.estudiante.apellido} - {self.periodo}'
-
-class Materia(models.Model):
-
-    nombre = models.CharField(max_length=30, unique=True, null=True)
-    literales = models.BooleanField(default=False)
+        return self.nombre  # Representación en cadena del objeto
 
     class Meta:
-        verbose_name = ("Materia")
-        verbose_name_plural = ("Materias")
-
-    def __str__(self):
-        return self.nombre
-
-class Carga(models.Model):
-
-    titulo = models.CharField(max_length=30, null=True)
-    materias = models.ManyToManyField(Materia)
-
-    class Meta:
-        verbose_name = ("Carga ")
-        verbose_name_plural = ("Cargas")
-
-    def __str__(self):
-        return self.titulo 
-
-class Periodo(models.Model):
-
-    fecha = models.CharField(max_length=30, null=True)
-    carga = models.ForeignKey('home.Carga', on_delete=models.RESTRICT, null=True)
-
-    class Meta:
-        verbose_name = ("Periodo")
-        verbose_name_plural = ("Periodos")
-
-    def __str__(self):
-        return f'{self.fecha} {self.carga.titulo}'
-
-class Nota(models.Model):
-
-    estudiante = models.ForeignKey('home.Estudiante', on_delete=models.CASCADE, null=True)
-    periodo = models.ForeignKey('home.Periodo', on_delete=models.RESTRICT, null=True)#Relación a periodo para agrupar notas las notas y mantener un registro histórico
-    materia = models.ForeignKey('home.Materia', on_delete=models.RESTRICT, null=True)
-    lapso_1 = models.CharField(max_length=2, validators=[MinLengthValidator(2), RegexValidator(r'^([0-1][0-9]|20|IN)$')], blank=True, null=True)
-    lapso_2 = models.CharField(max_length=2, validators=[MinLengthValidator(2), RegexValidator(r'^([0-1][0-9]|20|IN)$')], blank=True, null=True)
-    lapso_3 = models.CharField(max_length=2, validators=[MinLengthValidator(2), RegexValidator(r'^([0-1][0-9]|20|IN)$')], blank=True, null=True)
-    inasistencia_1 = models.IntegerField(validators=[int_list_validator(allow_negative=False)], default=0, blank=True, null=True)
-    inasistencia_2 = models.IntegerField(validators=[int_list_validator(allow_negative=False)], default=0, blank=True, null=True)
-    inasistencia_3 = models.IntegerField(validators=[int_list_validator(allow_negative=False)], default=0, blank=True, null=True)
-    reparacion = models.CharField(max_length=2, validators=[MinLengthValidator(2), RegexValidator(r'^([0-1][0-9]|20|IN)$')], blank=True, null=True)
-    #promedio = models.CharField(max_length=2, blank=True, null=True)
-
-    @property
-    def promedio(self):
-        lapsos = [self.lapso_1, self.lapso_2, self.lapso_3]
-        lapsos_numericos = [int(lapso) for lapso in lapsos if lapso.isdigit()]
-        if lapsos_numericos:
-            promedio = sum(lapsos_numericos) / len(lapsos_numericos)
-            return '{:02d}'.format(int(promedio))
-        return None
-    class Meta:
-        verbose_name = ("Nota")
-        verbose_name_plural = ("Notas")
-
-    def __str__(self):
-        return str(self.estudiante)
+        verbose_name_plural = "Datos Plantel"  # Nombre en plural para el panel de administración
