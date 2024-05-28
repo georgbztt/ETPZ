64cdcc2ff93f8052da679353c8441c2d3705d8f9
douglasmontoya <douglasmontoyalbjf@gmail.com>
2024-05-28 08:32:39 -0400

    Agrega 'DatosPlantel' para ser mostrado en la pagina de administracion

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from django.template.loader import get_template
 from django.contrib.admin.options import TabularInline
 
-from .models import *
+from .models import DatosPlantel
+
+admin.site.register(DatosPlantel)
