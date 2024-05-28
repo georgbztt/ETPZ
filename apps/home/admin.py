7f957f874a22c131c87413fcfac2bbf9897228c1
douglasmontoya <douglasmontoyalbjf@gmail.com>
2024-05-27 22:23:43 -0400

    Restaura el archivo admin.py

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -3,38 +3,3 @@ from django.template.loader import get_template
 from django.contrib.admin.options import TabularInline
 
 from .models import *
-"""
-class NotaInlineAdmin(admin.TabularInline):
-    model = Nota
-"""
-
-@admin.register(Boleta)
-class BoletaAdmin(admin.ModelAdmin):
-    'inlines = [NotaInlineAdmin]'
-    list_display = ('estudiante',)
-    fields = (
-            ('estudiante', ),
-            )
-"""     
-    readonly_fields = ('nota_inline',)
-    
-    def nota_inline(self, *args, **kwargs):
-        context = getattr(self.response, 'context_data', None) or {}
-        if context['inline_admin_formsets']:
-            inline = context['inline_admin_formset'] = context['inline_admin_formsets'].pop(0)
-            print(inline)
-            return get_template(inline.opts.template).render(context, self.request)#type:ignore
-        else:
-            print('inline is empty')
-            return get_template(inline.opts.template).render(self.request)#type:ignore
-    
-    def render_change_form(self, request, *args, **kwargs):
-        self.request = request
-        self.response = super().render_change_form(request, *args, **kwargs)
-        return self.response """
-    
-admin.site.register(Estudiante)
-admin.site.register(Periodo)
-admin.site.register(Carga)
-admin.site.register(Materia)
-admin.site.register(Nota)
