1b2485c347dd23794ffe84dd1d190c1b5e20efa6
douglasmontoya <douglasmontoyalbjf@gmail.com>
2024-05-28 20:12:34 -0400

    Agrega 'Secciones', 'AniosMencionSec', 'Anios' y 'Menciones' al admin

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.template.loader import get_template
 from django.contrib.admin.options import TabularInline
 
-from .models import DatosPlantel
+from .models import DatosPlantel, Secciones, AniosMencionSec, Anios, Menciones
 
 admin.site.register(DatosPlantel)
+admin.site.register(Secciones)
+admin.site.register(AniosMencionSec)
+admin.site.register(Anios)
+admin.site.register(Menciones)
