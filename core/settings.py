67264640ea305d7bfea12cb3879b4a3d5e604f12
douglasmontoya <douglasmontoyalbjf@gmail.com>
2024-05-31 07:44:42 -0400

    Establece DEGUB en True

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY', default='S#perS3crEt_007')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = True
 
 # Assets Management
 ASSETS_ROOT = os.getenv('ASSETS_ROOT', '/static/assets') 
