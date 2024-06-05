6cebd48bcbf0b167edd4d3ce70cb0124c7f03dd6
DADV20 <defreitasd2001@gmail.com>
2024-06-05 16:21:06 -0400

    Agregar modulo de estudiante y de carga de notas

--- apps/home/forms.py
+++ apps/home/forms.py
@@ -30,3 +30,16 @@ class AniosForm(forms.Form):
 class MencionesForm(forms.Form):
     nombre = forms.CharField(label="Nombre mención", max_length=255, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
     nombre_abrev = forms.CharField(label="Nombre mención abreviado", max_length=5, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    
+class EstudiantesForm(forms.Form):
+    ci = forms.IntegerField(label="Cédula", required=True, widget=forms.NumberInput(attrs={'class': 'form-control padding-left-10px'}))
+    ci_tipo = forms.ChoiceField(choices=[("V", "V"), ("E", "E")], required=True, widget=forms.Select(attrs={'class': 'form-control bg-light border-0 br-start'}))
+    nombres = forms.CharField(label="Nombres", max_length=255, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    apellidos = forms.CharField(label="Apellidos", max_length=255, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    sexo = forms.ChoiceField(choices=[("M", "M"), ("F", "F")], required=True, widget=forms.Select(attrs={'class': 'form-control text-center p-2'}))
+    fecha_de_nacimiento = forms.DateField(label="Fecha de nacimiento", required=True, widget=forms.DateInput(attrs={'class': 'form-control'}))
+    anio = forms.ChoiceField(label="Año", choices=[("1RO", "1RO"), ("2DO", "2DO"), ("3RO", "3RO"), ("4TO", "4TO"), ("5TO", "5TO"), ("6TO", "6TO"),], required=True, widget=forms.Select(attrs={'class': 'form-control text-center p-2'}))
+    mencion = forms.ChoiceField(label="Mención", choices=[("MM", "MM"), ("QI", "QI"), ("CC", "CC")], required=True, widget=forms.Select(attrs={'class': 'form-control text-center p-2'}))
+    seccion = forms.ChoiceField(label="Sección", choices=[("A", "A"), ("B", "B"), ("C", "C")], required=True, widget=forms.Select(attrs={'class': 'form-control text-center p-2'}))
+    entidad_federal = forms.CharField(label="Entidad federal", max_length=255, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    lugar_de_nacimiento =  forms.CharField(label="Lugar de nacimiento", max_length=255, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
